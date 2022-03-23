361136eb46098ee71e24f0623e0d1dfbc9659095
judygab <ivditigabeskiria@gmail.com>
2022-03-22 23:50:14 -0400

    added buttons in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,29 @@
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Button } from "react-bootstrap";
 
 interface HeaderProps {
   isLoggedIn?: boolean
 }
-export const Header = () => {
+export const Header: React.FC<HeaderProps> = (props) => {
+
+  const DisplayButtons = () => {
+    if (props.isLoggedIn) {
+      return (
+        <Button>Logout</Button>
+      )
+    } else {
+      return (
+        <Button>Login</Button>
+      )
+    }
+  }
+
   return (
     <Navbar>
       <Container>
         <Navbar.Brand href="#home">Logo</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">First Last</a>
-          </Navbar.Text>
+          <DisplayButtons />
         </Navbar.Collapse>
       </Container>
     </Navbar>
