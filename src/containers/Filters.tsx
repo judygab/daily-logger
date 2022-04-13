2a24287c9fcf198bb3dbd3dc55e67a76aa99a876
judygab <ivditigabeskiria@gmail.com>
2022-04-12 23:39:20 -0400

    added dropdown wrapper

--- src/containers/Filters.tsx
+++ src/containers/Filters.tsx
@@ -1,6 +1,8 @@
 import Box, { BoxProps } from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import MultipleSelectChip from "../components/MultiSelect";
+import { DropdownWrapper } from "../components/DropdownWrapper";
+import styles from "../../styles/Filters.module.css";
 
 export const Filters = () => {
   return (
@@ -12,7 +14,10 @@ export const Filters = () => {
       display: 'flex',
       justifyContent: 'space-between',
     }}>
-      <MultipleSelectChip />
+      <div className={styles.filtersContainer}>
+        <MultipleSelectChip />
+        <DropdownWrapper title="dropdown" />
+      </div>
       <Button disabled>Clear All</Button>
     </Box>
   )
