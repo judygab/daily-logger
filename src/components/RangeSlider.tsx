3567ccc66cefaa6930bc5898f1e6739a835fef23
judygab <ivditigabeskiria@gmail.com>
2022-04-22 18:17:54 -0400

    updated slider filter

--- src/components/RangeSlider.tsx
+++ src/components/RangeSlider.tsx
@@ -2,25 +2,21 @@ import React, { useState } from 'react';
 import Slider from '@mui/material/Slider';
 import Box from '@mui/material/Box';
 
-function valuetext(value: number) {
-  return `${value}°C`;
-}
-
 export const RangeSlider = ({ onHandleChange } : { onHandleChange : (updatedRange: Array<number>) => void}) => {
   const [value, setValue] = React.useState<number[]>([20, 37]);
 
   const handleChange = (event: Event, newValue: number | number[]) => {
     setValue(newValue as number[]);
+    onHandleChange(newValue as number[]);
   };
 
   return (
     <Box sx={{ width: 300, paddingTop: '30px' }}>
           <Slider
-            getAriaLabel={() => 'Temperature range'}
+            getAriaLabel={() => 'Range'}
             value={value}
             onChange={handleChange}
             valueLabelDisplay="on"
-            getAriaValueText={valuetext}
           />
     </Box>
   )
